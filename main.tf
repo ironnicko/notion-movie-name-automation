bb2339ea6e20934221ddea6fee258935c8c203c8
ironnicko <nickonicko779@gmail.com>
2024-11-27 15:32:15 +0530

    added Ansible Playbook

---
+++ main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.76.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region                   = "us-east-1"
+  shared_config_files      = ["/Users/nikhilivannan/.aws/config"]
+  shared_credentials_files = ["/Users/nikhilivannan/.aws/credentials"]
+
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 443
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 443
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    },
+    {
+      cidr_blocks      = ["0.0.0.0/0", ]
+      description      = ""
+      from_port        = 8000
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 8000
+    }
+  ]
+}
+
+resource "aws_instance" "my_vm" {
+  ami = "ami-055e62b4ea2fe95fd"
+
+  instance_type          = "t4g.nano"
+  key_name               = "vockey"
+  vpc_security_group_ids = [aws_security_group.main.id]
+  # user_data              = "${file("script.sh")}"
+
+  tags = {
+    Name = "first-tf-instance"
+  }
+
+}
+
