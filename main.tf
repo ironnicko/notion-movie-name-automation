b52b0f58aa92a4a2ad4c04cf28a9bd3f2e61b6d3
Nikhil I <66690978+ironnicko@users.noreply.github.com>
2024-11-28 23:48:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ terraform {
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_config_files      = ["/Users/nikhilivannan/.aws/config"]
-  shared_credentials_files = ["/Users/nikhilivannan/.aws/credentials"]
+  shared_config_files      = ["~/.aws/config"]
+  shared_credentials_files = ["~/.aws/credentials"]
 
 }
 
